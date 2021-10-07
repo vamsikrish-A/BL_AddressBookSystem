6f9a9a85c36d5f3dc737054fcab5459682402771
vamsikrish-A <krishnavamsi165@gmail.com>
2021-10-07 13:04:10 +0530

    Welcome message

---
+++ src/main/java/AddressBookSystem.java
@@ -0,0 +1,12 @@
+/*
+ * @Project : AddressBookSystem using FileIO
+ *
+ * @created by : Vamsi Krishna A
+ * @ 07-10-2021
+ */
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBookSystem.");
+    }
+}
